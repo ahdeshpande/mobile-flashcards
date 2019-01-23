7cd9037134b44660811d882b1dce0c4ab7d492b9
ahdeshpande <deshpande.abhiraj@gmail.com>
2019-01-23 10:29:29 +0530

    feat: Remove firebase

--- utils/_DATA.js
+++ utils/_DATA.js
@@ -1,37 +1,96 @@
-import {firebaseConfig} from "./firebase";
-import * as firebase from "firebase";
-import firestore from "firebase/firestore";
-
-const settings = {timestampsInSnapshots: true};
-
-firebase.initializeApp(firebaseConfig);
-
-firebase.firestore().settings(settings);
+let data = {
+    React: {
+        id: "React",
+        title: 'React',
+        questions: [
+            {
+                id: "q_asdf723r8yf78347",
+                question: 'What is React?',
+                answer: 'A library for managing user interfaces'
+            },
+            {
+                id: "q_093487635879fsdvds",
+                question: 'Where do you make Ajax requests in React?',
+                answer: 'The componentDidMount lifecycle event'
+            }
+        ]
+    },
+    JavaScript: {
+        id: "Javascript",
+        title: 'JavaScript',
+        questions: [
+            {
+                id: "q_867123g8g345vg7",
+                question: 'What is a closure?',
+                answer: 'The combination of a function and the lexical environment within which that function was declared.'
+            }
+        ]
+    },
+};
 
 export function _getDecks() {
-    // return new Promise((res,) => {
-    //
-    //     firebaseApp.database().ref()
-    //         .child('decks')
-    //         .on('value', (snapshot) => {
-    //
-    //             // get children as an array
-    //             let decks = [];
-    //             snapshot.forEach((child) => {
-    //                 decks.push({
-    //                     deckName: child.val().deckName,
-    //                     cardCount: child.val().cardCount,
-    //                     _key: child.key,
-    //                 });
-    //             });
-    //
-    //             res(decks);
-    //         }, (errorObject) => {
-    //             console.log("The read failed: " + errorObject.code);
-    //             return {};
-    //         });
-    // });
-    return firebase.firestore().collection('decks');
+    return new Promise((res, rej) => {
+        setTimeout(() => res({...data}), 1000)
+    })
 }
 
-export default firebase;
+function generateUID() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15)
+}
+
+function formatDeck({title}) {
+    return {
+        id: generateUID(),
+        title: title,
+        questions: [],
+    }
+}
+
+function formatQuestion({question, answer}) {
+    return {
+        id: 'q_' + generateUID(),
+        question: question,
+        answer: answer,
+    }
+}
+
+
+export function _saveDeck({title}) {
+    return new Promise((res, rej) => {
+
+        const formattedDeck = formatDeck({
+            title,
+        });
+
+        setTimeout(() => {
+            data = {
+                ...data,
+                [formattedDeck.id]: formattedDeck,
+            };
+
+            res(formattedDeck);
+        }, 1000);
+    });
+}
+
+export function _saveQuestion({deckId, question, answer}) {
+    return new Promise((res, rej) => {
+        const formattedQuestion = formatQuestion({
+            question,
+            answer,
+        });
+
+        setTimeout(() => {
+
+            data = {
+                ...data,
+                [deckId]: {
+                    ...data[deckId],
+                    questions: data[deckId].questions.concat([formattedQuestion]),
+                }
+            };
+
+            res(formattedQuestion);
+        }, 1000)
+    });
+}
--- utils/api.js
+++ utils/api.js
@@ -1,13 +1,5 @@
 import {_getDecks} from "./_DATA";
 
-// export function getInitialData() {
-//     return Promise.all([
-//         _getDecks(),
-//     ]).then(([decks]) => ({
-//         decks,
-//     }));
-// }
-
 export function getInitialData() {
     return Promise.all([
         _getDecks(),
