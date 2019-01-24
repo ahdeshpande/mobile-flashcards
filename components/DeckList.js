49bd603dbcc476df372137de04e4df01d70508f2
ahdeshpande <deshpande.abhiraj@gmail.com>
2019-01-24 23:13:45 +0530

    feat: Add refresh to decklist

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet, RefreshControl} from 'react-native';
 import {white} from "../utils/colors";
 import Deck from "./Deck";
 import {connect} from "react-redux";
 import {handleInitialData} from "../actions";
 
 class DeckList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            refreshing: false,
+        };
+    }
+
+    _onRefresh = () => {
+        this.setState({refreshing: true});
+        this.props.dispatch(handleInitialData())
+            .then(() => {
+                this.setState({refreshing: false});
+            });
+    };
 
     renderItem = (deck) => {
         const {title, cardCount, id} = deck.item;
@@ -29,6 +44,12 @@ class DeckList extends Component {
         return (
             <View style={styles.container}>
                 {decks && <FlatList
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.state.refreshing}
+                            onRefresh={this._onRefresh}
+                        />
+                    }
                     data={decks}
                     renderItem={this.renderItem}
                     keyExtractor={(deck) => deck.id}
