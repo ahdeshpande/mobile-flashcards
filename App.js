e41d130daf45f11ccb3b9f2225e410e6c68ee7d0
ahdeshpande <deshpande.abhiraj@gmail.com>
2019-01-24 16:34:28 +0530

    feat: Create Home Screen

--- App.js
+++ App.js
@@ -1,63 +1,43 @@
 import React from 'react';
-import {AppRegistry, StyleSheet, View, Text, StatusBar} from 'react-native';
-import {Constants} from 'expo';
 import {Provider} from "react-redux";
 import {applyMiddleware, createStore} from "redux";
 import reducer from './reducers';
-import DeckList from "./components/DeckList";
 import thunk from 'redux-thunk';
-import {adBlue, purple} from "./utils/colors";
+import Home from "./components/Home";
+
+import {
+    createAppContainer,
+    createStackNavigator
+} from 'react-navigation';
 
 
-function FCStatusBar({backgroundColor, ...props}) {
-    return (
-        <View style={{backgroundColor, height: Constants.statusBarHeight}}>
-            <StatusBar translucent
-                       backgroundColor={backgroundColor} {...props}/>
-        </View>
-    )
-}
+const MainNavigator = createStackNavigator({
+    Home: {
+        screen: Home,
+    },
+    // DeckDetail: {
+    //     screen: EntryDetail,
+    //     navigationOptions: {
+    //         headerTintColor: white,
+    //         headerStyle: {
+    //             backgroundColor: purple,
+    //         }
+    //     }
+    // }
+});
+
+const AppContainer = createAppContainer(MainNavigator);
 
 class App extends React.Component {
 
     render() {
         return (
             <Provider store={createStore(reducer, applyMiddleware(thunk))}>
-                <View style={styles.container}>
-                    <FCStatusBar backgroundColor={adBlue}
-                                 barStyle={'light-content'}/>
-                    <View style={styles.appHeader}>
-                        <Text style={styles.appName}>
-                            Flashcards
-                        </Text>
-                    </View>
-                    <DeckList style={styles.deckList}/>
-                </View>
+                <AppContainer/>
             </Provider>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-    },
-    deckList: {
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    appHeader: {
-        padding: 5,
-        backgroundColor: adBlue,
-    },
-    appName: {
-        fontSize: 28,
-        fontWeight: 'bold',
-        color: '#fff',
-        textAlign: 'center',
-    }
-});
 
 
 export default App;
--- components/Deck.js
+++ components/Deck.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import {white} from "../utils/colors";
 
 class Deck extends Component {
 
     render() {
-        const {deckName, cardCount} = this.props;
+        const {deckName, cardCount, id} = this.props;
 
         return (
             <View style={styles.container}>
-                <Text style={styles.header}>
-                    {deckName}
-                </Text>
-                <Text style={styles.counter}>
-                    {cardCount} card{cardCount !== 1 && 's'}
-                </Text>
+                <TouchableOpacity onPress={() => {
+                    // this.props.navigation.navigate(
+                    //     'EntryDetail',
+                    //     {entryId: key}
+                    // );
+                }}>
+                    <Text style={styles.header}>
+                        {deckName}
+                    </Text>
+                    <Text style={styles.counter}>
+                        {cardCount} card{cardCount !== 1 && 's'}
+                    </Text>
+                </TouchableOpacity>
             </View>
         )
     }
@@ -26,6 +33,8 @@ const styles = StyleSheet.create({
         padding: 20,
         backgroundColor: white,
         textAlign: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: '#000',
     },
     header: {
         fontSize: 36,
